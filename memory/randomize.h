1e1a3909f6bea03e8135890ad9dcdd3cf2b31eb7
Warren MacEvoy <wmacevoy@gmail.com>
2024-03-03 10:04:32 -0700

    random win32 fix

--- memory/randomize.h
+++ memory/randomize.h
@@ -6,15 +6,15 @@
 #pragma comment(lib, "bcrypt.lib") // Link against the bcrypt library
 template <typename Primitive>
 void randomize(Primitive &primitive) {
-    std::ifstream random("/dev/random", std::ios::in | std::ios::binary);
-    NTSTATUS status = BCryptGenRandom(NULL, reinterpret_cast<PUCHAR>(&primitive), sizeof(Primitive), BCRYPT_USE_SYSTEM_PREFERRED_RNG);
-    if (status != ERROR_SUCCESS) std::abort();
+  if (BCryptGenRandom(NULL, reinterpret_cast<PUCHAR>(&primitive), sizeof(Primitive), BCRYPT_USE_SYSTEM_PREFERRED_RNG) != ERROR_SUCCESS) {
+    std::abort();
+  }
 }
 #else
 #include <fstream>
 template <typename Primitive>
 void randomize(Primitive &primitive) {
-    std::ifstream random("/dev/random", std::ios::in | std::ios::binary);
+    static std::ifstream random("/dev/random", std::ios::in | std::ios::binary);
     if (!random.read(reinterpret_cast<char*>(&primitive), sizeof(Primitive))) {
       std::abort();
     }
